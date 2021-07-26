df001b0f15159c6f07b4b1eeec34454ed08f7320
Marialyn08 <marialyn.lizarondo@evsu.edu.ph>
2021-07-26 15:34:11 +0800

    contact

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyC_2qBXWfZf0cksDmZKe9EOgKOciUWl5hw",
+      authDomain: "activity5-mygallery.firebaseapp.com",
+      projectId: "activity5-mygallery",
+      storageBucket: "activity5-mygallery.appspot.com",
+      messagingSenderId: "783581199175",
+      appId: "1:783581199175:web:58e96c99cb4368d9f4ea62",
+      measurementId: "G-CSBLY28ZD0"
+  }
+
 };
 
 /*
